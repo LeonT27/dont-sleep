1db853d0b225bdd2660c3a145c8e10819bb76730
ltolentino <luistolentino27@gmail.com>
2022-03-07 21:01:47 -0400

    adding init and clearCounter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,46 @@
 "use strict";
 
-function counter(s) {
-  let seconds = s;
-  setInterval(() => {
+//INIT
+let intervalId = counter();
+let ds = "wakeLock" in navigator ? true : null;
+
+(async () => {
+  await dontSleep();
+})();
+
+//VisibilityChange
+document.addEventListener("visibilitychange", async () => {
+  if (ds !== null && document.visibilityState === "visible") {
+    await dontSleep();
+    intervalId = counter();
+  }
+
+  if (document.visibilityState === "hidden") {
+    clearCounter(intervalId);
+  }
+});
+
+//FUNC
+async function dontSleep() {
+  try {
+    let ds = await navigator.wakeLock.request("screen");
+    text_color("true");
+  } catch (error) {
+    text_color("false");
+    clearCounter(intervalId);
+    document.getElementById("count").innerHTML = `<p>Refresh the page plz.</p>`;
+  }
+}
+
+let text_color = (nameClass) => {
+  let body = document.getElementsByTagName("BODY")[0];
+  body.classList.remove(...body.classList);
+  body.classList.add(nameClass);
+};
+
+function counter() {
+  let seconds = 0;
+  return setInterval(() => {
     seconds += 1;
     document.getElementById(
       "count"
@@ -10,18 +48,9 @@ function counter(s) {
   }, 1000);
 }
 
-counter(0);
-//TODO dontSleep on init
-let dontSleep = "wakeLock" in navigator ? true : null;
-
-document.getElementsByTagName("BODY")[0].classList.add(dontSleep);
-
-console.log(dontSleep);
-
-document.addEventListener("visibilitychange", async () => {
-  if (dontSleep !== null && document.visibilityState === "visible") {
-    dontSleep = await navigator.wakeLock.request("screen");
-
-    console.log(dontSleep);
-  }
-});
+function clearCounter(intervalId) {
+  clearInterval(intervalId);
+  document.getElementById(
+    "count"
+  ).innerHTML = `<p>You have been here for 0 seconds.</p>`;
+}
