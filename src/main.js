33da827303fa3bbe3db3dc9afce97abec09576b0
ltolentino <luistolentino27@gmail.com>
2022-03-21 19:58:05 -0400

    changing to vanilla js project setup

--- src/main.js
+++ src/main.js
@@ -1,56 +1,70 @@
 "use strict";
 
-//INIT
-let intervalId = counter();
-let ds = "wakeLock" in navigator ? true : null;
-
-(async () => {
-  await dontSleep();
-})();
-
-//VisibilityChange
-document.addEventListener("visibilitychange", async () => {
-  if (ds !== null && document.visibilityState === "visible") {
-    await dontSleep();
-    intervalId = counter();
-  }
-
-  if (document.visibilityState === "hidden") {
-    clearCounter(intervalId);
-  }
-});
-
-//FUNC
-async function dontSleep() {
-  try {
-    let ds = await navigator.wakeLock.request("screen");
-    text_color("true");
-  } catch (error) {
-    text_color("false");
-    clearCounter(intervalId);
-    document.getElementById("count").innerHTML = `<p>Refresh the page plz.</p>`;
-  }
-}
-
-let text_color = (nameClass) => {
-  let body = document.getElementsByTagName("BODY")[0];
-  body.classList.remove(...body.classList);
-  body.classList.add(nameClass);
+let state = {
+  seconds: 0,
+  counterId: 0,
+  dontSleep: false,
 };
 
-function counter() {
-  let seconds = 0;
-  return setInterval(() => {
-    seconds += 1;
-    document.getElementById(
-      "count"
-    ).innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
-}
+let incrementSeconds = () => state.seconds++;
+let resetSeconds = () => (state.seconds = 0);
+let runCounter = () => (state.counterId = counter());
+let setDontSleep = async () => (state.dontSleep = await dontSleep());
 
-function clearCounter(intervalId) {
+let $D = document;
+let $body = $D.getElementsByTagName("BODY")[0];
+let $counterText = $D.getElementById("counter-text");
+let $counter = $D.getElementById("counter");
+let $error = $D.getElementById("error");
+
+let setTextColor = (nameClass) => {
+  $body.classList.remove(...$body.classList);
+  $body.classList.add(nameClass);
+};
+let updateCounterView = () => ($counter.innerHTML = state.seconds);
+let resetCounterView = () => ($counter.innerHTML = 0);
+let showErrors = () => {
+  $counterText.classList.add("hidden");
+  $error.classList.toggle("hidden");
+};
+
+let onVisibilityChange = async () => {
+  if (state.dontSleep !== null && $D.visibilityState === "visible") {
+    await dontSleep();
+    runCounter();
+  }
+
+  if ($D.visibilityState === "hidden") {
+    clearCounter(state.counterId);
+  }
+};
+
+let counter = () => {
+  return setInterval(() => {
+    incrementSeconds();
+    updateCounterView();
+  }, 1000);
+};
+
+let clearCounter = (intervalId) => {
+  resetSeconds();
   clearInterval(intervalId);
-  document.getElementById(
-    "count"
-  ).innerHTML = `<p>You have been here for 0 seconds.</p>`;
-}
+  resetCounterView();
+};
+
+let dontSleep = async () => {
+  try {
+    setTextColor("true");
+  } catch (error) {
+    setTextColor("false");
+    clearCounter(state.counterId);
+    showErrors();
+    return false;
+  }
+  return true;
+};
+
+$D.onvisibilitychange = async () => onVisibilityChange();
+
+runCounter();
+setDontSleep();
